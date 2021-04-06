34ca1e0eeebff16a4ea88f99d8c452f3a846d82e
lemamon <demmarinho@gmail.com>
2021-04-06 18:10:46 -0300

    feat: added new styles to use theme

--- src/config/theme.ts
+++ src/config/theme.ts
@@ -1,5 +1,6 @@
 import {Colors} from 'react-native/Libraries/NewAppScreen'
 import {AppThemeProps, ThemeProps} from '../types'
+import {homeStyles, itemStyle, detailsStyle, simpleListStyle} from '../styles'
 
 const useAppTheme = ({isDarkMode}: ThemeProps): AppThemeProps => {
   return {
@@ -8,6 +9,10 @@ const useAppTheme = ({isDarkMode}: ThemeProps): AppThemeProps => {
     borderColor: isDarkMode ? Colors.white : Colors.black,
     headerTintColor: isDarkMode ? Colors.white : Colors.black,
     barStyle: isDarkMode ? 'light-content' : 'dark-content',
+    homeStyles,
+    itemStyle,
+    detailsStyle,
+    simpleListStyle,
   }
 }
 
