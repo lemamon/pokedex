095bc17bc6672dc10828db9683b3a37f8f399e50
lemamon <demmarinho@gmail.com>
2021-04-06 16:11:25 -0300

    feat: created new types

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,3 +1,6 @@
+import {StackNavigationProp} from '@react-navigation/stack'
+import {StatusBarStyle} from 'react-native'
+
 export type Pokemon = {
   name: string
   url: string
@@ -16,4 +19,31 @@ export type ListProps = {
 export type ItemProps = {
   item: Pokemon
   onPressItem: Function
+}
+
+export enum AppScreens {
+  Home = 'Home',
+  Details = 'Details',
+}
+
+export type Props = {
+  navigation: StackNavigationProp<any, any>
+  route: any
+}
+
+export type ThemeProps = {
+  isDarkMode: boolean
+}
+
+export type SimpleListProps = {
+  data: string[]
+  title: string
+}
+
+export type AppThemeProps = {
+  backgroundColor: string
+  color: string
+  borderColor: string
+  headerTintColor: string
+  barStyle: StatusBarStyle
 }
