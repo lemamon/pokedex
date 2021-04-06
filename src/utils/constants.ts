de956add9cfcc4250a8ad5b19934768966780b4b
lemamon <demmarinho@gmail.com>
2021-04-06 16:11:00 -0300

    feat: added constants

---
+++ src/utils/constants.ts
@@ -0,0 +1,6 @@
+const BASE_URL = 'https://pokeapi.co/api/v2/'
+const LIMIT_TO_GET = 30
+const PATH_URL = `pokemon?limit=${LIMIT_TO_GET}`
+const DARK_THEME = 'dark'
+
+export {BASE_URL, PATH_URL, LIMIT_TO_GET, DARK_THEME}
